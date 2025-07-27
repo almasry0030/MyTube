1a036f467cf8ad58ca2d6b78ce2d3ebf9199466d
almasry0030 <bdwalfywmy126@gmail.com>
2025-07-27 09:54:57 +0300

    Add files via upload
    Signed-off-by: almasry0030 <bdwalfywmy126@gmail.com>

---
+++ slider.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import * as SliderPrimitive from '@radix-ui/react-slider';
+import { cn } from '@/lib/utils';
+
+const Slider = React.forwardRef(({ className, ...props }, ref) => (
+  <SliderPrimitive.Root
+    ref={ref}
+    className={cn(
+      'relative flex w-full touch-none select-none items-center',
+      className
+    )}
+    {...props}
+  >
+    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-gray-600">
+      <SliderPrimitive.Range className="absolute h-full bg-red-600" />
+    </SliderPrimitive.Track>
+    <SliderPrimitive.Thumb className="block h-3 w-3 rounded-full border border-red-600 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-red-600 disabled:pointer-events-none disabled:opacity-50" />
+  </SliderPrimitive.Root>
+));
+Slider.displayName = SliderPrimitive.Root.displayName;
+
+export { Slider };
